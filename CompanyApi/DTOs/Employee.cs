61b5a81374f3ad088da551841430afb91f44e5f8
Jingbo Yu <Jyu20@slb.com>
2023-11-06 19:41:46 +0800

    feat: design CRUD api about employee and D api for company

---
+++ CompanyApi/DTOs/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyApi.DTOs
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Position {  get; set; }
+    }
+}
